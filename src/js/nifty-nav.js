65c082063ad5b46f890b8582713c058958786e60
Eric Stout <ericwstout@gmail.com>
2016-02-08 09:32:15 -0700

    Adds new JS for nifty-nav.js

--- src/js/nifty-nav.js
+++ src/js/nifty-nav.js
@@ -4,72 +4,76 @@
 * Repo: https://github.com/factor1/nifty-nav
 */
 
-// function we use to remove the mask
-var niftyRemove = function(){
-	$('.nifty-mask').remove();
+var niftyNav = function(options){
+  $nifty_toggle = $('#nifty-nav-toggle');
+  $nifty_panel = $('.nifty-panel');
+  $nifty_nav_item = $('.nifty-nav-item');
+  $nifty_parent = $('.nifty-panel ul li');
+
+  var settings = $.extend({
+    // These are the defaults.
+    sub_menus: false,
+    use_mask: true,
+    item_click_close: true
+  }, options );
+
+  sub_menus         = settings.sub_menus;
+  use_mask          = settings.use_mask;
+  item_click_close  = settings.item_click_close;
+
+
+  // Core Nifty Nav Functions
+  niftyRemove = function(){
+    $('.nifty-mask').remove();
+  };
+
+  niftyUnmask = function(){
+    $('.nifty-mask').animate({opacity: 0.0});
+    setTimeout(niftyRemove, 800);
+  };
+
+  // on nifty nav toggle click
+  $nifty_toggle.click(function(){
+    var $this = $(this);
+    $this.toggleClass('nifty-active');
+    $nifty_panel.slideToggle(500).css('position','absolute');
+
+    if( use_mask === true){
+      // if a mask exists
+      if( $('.nifty-mask').length > 0 ){
+        niftyUnmask();
+      } else{
+        // if no mask exists
+        $('body').append('<div class="nifty-mask"></div>');
+        $('.nifty-mask').animate({opacity: 1.0});
+
+        // if a user clicks on the mask
+        $('.nifty-mask').click(function(){
+          $nifty_panel.slideUp(500);
+          niftyUnmask();
+          $nifty_toggle.removeClass('nifty-active');
+        });
+      }
+    }
+  });
+
+  // close nifty nav on nav item click
+  if( item_click_close === true ){
+    $nifty_nav_item.click(function(){
+      $nifty_panel.slideUp(500);
+      niftyUnmask();
+      $nifty_toggle.removeClass('nifty-active');
+    });
+  }
+
+  // if sub menus are enabled
+  if( sub_menus === true ){
+    var $nifty_parent_active;
+    $nifty_parent.click(function(){
+      $nifty_parent_active = $(this);
+      $nifty_parent_active.find('.sub-menu').slideToggle();
+      $nifty_parent_active.find('a').toggleClass('nifty-menu-opened');
+    });
+  }
+
 };
-
-var niftyUnMaskIt = function(){
-	$('.nifty-mask').animate({opacity:0.0});
-	setTimeout(niftyRemove, 800);
-};
-
-// setting up our variables for submenu toggles
-var mobileNavItem = $('.nifty-panel ul li');
-var $mobileClicked;
-
-// when the document is ready lets go!
-$(document).ready(function(){
-	i = 0;
-	// listen for the click on the menu icon
-	$('#nifty-nav-toggle').click(function(){
-		// Add 1 to the count
-		i++;
-		$('#nifty-nav-toggle').toggleClass('nifty-active');
-		// toggle the nav up/down
-		$('.nifty-panel').slideToggle(500).css("position", "absolute");
-		// Check if the counter is even or odd
-		var isEven = function(clickCounter){
-			return (clickCounter%2 === 0) ? true : false;
-		};
-
-		if (isEven(i) === false){
-			//lets create a nice mask to dim the content
-			$('body').append('<div class="nifty-mask"></div>');
-			$('.nifty-mask').animate({opacity:1.0});
-		} else {
-			niftyUnMaskIt();
-		}
-
-		// mask click collapses the nav
-		$('.nifty-mask').click(function(){
-			//slide up the nav panel
-			$('.nifty-panel').slideUp(500);
-			//remove the mask
-			niftyUnMaskIt();
-			// remove the active class on the hamburger mmmm hamburger
-			$('#nifty-nav-toggle').removeClass('nifty-active');
-			i++;  // Add 1 to the count
-		});
-	});
-	// Close the nav when a nav item is clicked
-	$('.nifty-nav-item').click(function(){
-		//slide up our panel
-		$('.nifty-panel').slideUp(500);
-		niftyUnMaskIt();
-		$('#nifty-nav-toggle').removeClass('nifty-active');
-		i = 0;  // reset the counter
-	});
-
-	/*
-	Listen for any menu items that have children and allow them
-	to toggle open when clicked. This is by default setup for
-	Wordpress menus, however if you're not using Wordpress just
-	make sure your classes and structure matches.
-	*/
-	mobileNavItem.on('click', function(){
-		$mobileClicked = $(this);
-		$mobileClicked.find('.sub-menu').slideToggle();
-		$mobileClicked.find('a').toggleClass('menu-opened');
-	});
-});
